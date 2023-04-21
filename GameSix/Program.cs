1fb71a5051783ae88372cea108bf1112ec2decbb
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-04-21 11:07:07 +0100

    Add pedir nome do inimigo

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,27 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Write the number of enemies");
+            int EnemyNumber=int.Parse( Console.ReadLine());
+
+            Foe[] enemies=new Foe[EnemyNumber];
+
+            for (int e=0;e<EnemyNumber;e++)
+            {
+                Console.WriteLine("Write the enemy name");
+                string name= Console.ReadLine();
+                Foe ene= new Foe(name);
+                enemies[e]=ene;
+            }
+
+            Console.WriteLine("Enemies:");
+
+            foreach (Foe enemy in enemies)
+            {
+                Console.WriteLine(enemy.GetName());
+            }
+
+            
         }
     }
 }
